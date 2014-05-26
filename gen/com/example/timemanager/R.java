a2c5e382d912daea771f9e1bd529524bcb1cd9b5
claudivanfilho <j.claudivan.filho@gmail.com>
2014-05-26 16:26:25 -0300

    campos de entrada do usuario implementados

--- gen/com/example/timemanager/R.java
+++ gen/com/example/timemanager/R.java
@@ -28,7 +28,13 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090000;
+        public static final int action_settings=0x7f090006;
+        public static final int campoAtividade=0x7f090003;
+        public static final int campoHoras=0x7f090002;
+        public static final int digitalClock1=0x7f090001;
+        public static final int textView1=0x7f090000;
+        public static final int textView2=0x7f090004;
+        public static final int textView3=0x7f090005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -39,8 +45,10 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int atividade=0x7f050005;
         public static final int bem_vindo=0x7f050002;
         public static final int hello_world=0x7f050003;
+        public static final int horas=0x7f050004;
     }
     public static final class style {
         /** 
